43223dd12328796f337cc42a66c7ed50889d015f
Udayan Tandon <udayan12167@iiitd.ac.in>
2015-08-11 15:06:50 +0200

    Add ProjectMetadata

---
+++ source/support/ProjectMetadata.py
@@ -0,0 +1,22 @@
+import shelve
+import time
+import platform
+import os
+
+class ProjectMetadata:
+    def __init__(self):
+        self.file = os.path.expanduser('~/.local/.coala')
+        if platform.system() == 'windows':# pragma: no cover
+            self.file = os.getenv("APPDATA") + '\\coala\\.coala'
+        self.shelf = None
+
+    def add_project(self, name, location):
+        self.shelf = shelve.open(self.file)
+        timestamp = time.localtime()
+        self.shelf[name] = (timestamp, location)
+        self.shelf.close()
+
+    def delete_project(self, name):
+        self.shelf = shelve.open(self.file)
+        del self.shelf[name]
+        self.shelf.close()
